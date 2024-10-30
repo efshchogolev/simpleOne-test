99601404555ff4e08b20f0622d6a923c7e7e728b
Efim Shchegolev <efimblashck@yandex.ru>
2024-10-30 09:43:10 +0300

    feat: change max-width / add Menu component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import Header from '../Header/Header'
+import s from './App.module.css'
 
 function App() {
   return (
-    <>
+    <div className={s.app}>
       <Header />
-    </>
+    </div>
   )
 }
 
